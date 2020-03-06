46a9b74ea0b7f675020b11365d528aba3b421e70
Andrew S <andrews54757@gmail.com>
2020-03-06 09:46:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -636,7 +636,7 @@ function updateServer(server) {
 
 
         var time = Date.now();
-        for (var i = 0; i < server.elements.lineData.length - 1; i++) {
+        for (var i = 0; i < server.elements.lineData.length - 2; i++) {
             if (time - server.elements.lineData[i].x > 1000 * 60 * 60 * 1) {
                 server.elements.lineData.splice(i, 1);
                 i--;
