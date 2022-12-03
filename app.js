5eb1500cc1a91db1d8ff4347e321f71f243a4b71
Andrew S <andrews54757@gmail.com>
2022-12-03 01:54:29 -0600

    RIP Heroku

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ Explore at your own risk!
 
 
 // Proxy server since browser can't do raw TCP
-var SERVER_URL = "https://minecraft-status-observer.herokuapp.com";
+var SERVER_URL = "https://mc-observer-server.glitch.me";
 
 var SERVER_STATUS = "";
 
